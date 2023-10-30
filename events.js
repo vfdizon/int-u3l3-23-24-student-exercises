7c3d4c4546856942d54be290200c4a4bb246c6a3
vfdizon <vdizon@cps.edu>
2023-10-30 15:21:16 +0000

    did all tasks

--- events.js
+++ events.js
@@ -10,54 +10,68 @@ let changeColorButton = document.querySelector(".change-color");
 let endGameButton = document.querySelector(".end-game");
 let updateScoreButton = document.querySelector(".update-score");
 let growButton = document.querySelector(".grow");
+const colorsMap = new Map([
+	["skyblue", "red"],
+	["red", "orange"],
+	["orange", "yellow"],
+	["yellow", "green"],
+	["green", "blue"],
+	["blue", "indigo"],
+	["indigo", "violet"],
+	["violet", "red"]
+]);
 
-/* 1. When jump button is clicked, Mario jumps and coin disappears.  
 
-________.addEventListener("click", function() {
-    mario.style.bottom = "120px";
-    coin.style.display = "none";
+// /* 1. When jump button is clicked, Mario jumps and coin disappears.  
+
+jumpButton.addEventListener("click", function() {
+	mario.style.bottom = "120px";
+	coin.style.display = "none";
 });
 
-*/
 
 
-/* 2. When jump down button is clicked, Mario jumps down. 
+// /* 2. When jump down button is clicked, Mario jumps down. 
 
-________.addEventListener("click", function() {
-    mario.style = "bottom: 35px";
+jumpDownButton.addEventListener("click", function() {
+	mario.style = "bottom: 35px";
 });
 
-*/
 
 
-/* 3. When change color button is clicked, background color changes. 
+// /* 3. When change color button is clicked, background color changes. 
 
-________.addEventListener("click", function() {
-    background.style.backgroundColor = "skyblue";
+changeColorButton.addEventListener("click", function() {
+	console.log(background.style.backgroundColor);
+	if(background.style.backgroundColor === "") {
+		background.style.backgroundColor = "red";
+	}
+	
+	background.style.backgroundColor = colorsMap.get(background.style.backgroundColor);
+	
 });
 
-*/
 
 
-/* 4. When end game button is clicked, the title of the page says game over. 
+// /* 4. When end game button is clicked, the title of the page says game over. 
 
-________._______ ("___", ________()
-    title.innerHTML = "Game over.";
+endGameButton.addEventListener("click", function() {
+		title.innerHTML = "Game over.";
 });
 
-*/
 
 
-/* 5. When update score button is clicked, the counter score becomes "1" instead of 0. 
+// /* 5. When update score button is clicked, the counter score becomes "1" instead of 0. 
 
-________._______ ("___", ________()
-    score.innerHTML = 1;
+updateScoreButton.addEventListener("click", function() {
+	let currentScore = Number(score.innerHTML);
+	score.innerHTML = currentScore + 1;
 });
 
-*/
 
 
-/*6: When clicking the "Grow" button the Mario image gets bigger. Write the whole click handler! 
-
-
-*/
+// /*6: When clicking the "Grow" button the Mario image gets bigger. Write the whole click handler! 
+growButton.addEventListener("click", function() {
+	mario.style.width = "240px";
+	mario.style.height = "240px";
+});
--- script.js
+++ script.js
@@ -1,16 +1,13 @@
 let button = document.querySelector("button");
-                                    
+console.log(button);
 // 1. Declare a variable called `answer` that stores the p tag that has the class name `answer`.
-
+let answer = document.querySelector(".answer");
 
 
 button.addEventListener("click", function() {
-// 2. Replace the value with a string with a name of a movie you enjoy.
-// 3. Uncomment the line of code below, run, and try the button.
-// answer.innerHTML="movie name goes here";
 
+	// 2. Replace the value with a string with a name of a movie you enjoy.
 
-
-
-
+	// 3. Uncomment the line of code below, run, and try the button.
+	answer.innerHTML = "Pacific Rim";
 });
